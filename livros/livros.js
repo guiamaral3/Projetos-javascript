a197a590df39cb6e4465e435572e104362aed1bd
Guilherme Silva <guilherme.silva@qsaude.com.br>
2023-01-23 09:48:41 -0300

    Organizar pastas e andamento de aula 5 e 6 de NodeJS

--- livros/livros.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get('/api', (req, res) => {
-    res.send({ posso: 'ir embora?'});
-});
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
