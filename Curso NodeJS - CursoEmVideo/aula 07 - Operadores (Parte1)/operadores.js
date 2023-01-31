da4787f10e0b9cacbad4c61cd65ea6a9287c2557
Guilherme Silva <guilherme.silva@qsaude.com.br>
2023-01-31 09:05:38 -0300

    Add aula 07

---
+++ Curso NodeJS - CursoEmVideo/aula 07 - Operadores (Parte1)/operadores.js
@@ -0,0 +1,27 @@
+/* Operadores aritméticos
+
+Soma
+5 + 2 = 7
+
+Subtração
+5 - 2 = 7
+
+Multiplicação
+5 * 2 = 10
+
+Divisão
+5 / 2 = 2,5
+
+Mod - resto da divisão
+5 % 2 = 1
+
+Potencia
+5 ** 2 = 25
+*/
+
+/* Ordem de precedência
+()
+**
+* / %
++ -
+*/
