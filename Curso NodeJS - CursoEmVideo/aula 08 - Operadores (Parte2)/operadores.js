e290059923a5b70a7242ad46f39300011804caee
Guilherme Silva <guilherme.silva@qsaude.com.br>
2023-02-02 11:12:49 -0300

    Add aula 08

---
+++ Curso NodeJS - CursoEmVideo/aula 08 - Operadores (Parte2)/operadores.js
@@ -0,0 +1,30 @@
+/* Ordem de precedencia
+
+Aritméticos
+()
+**
+* / %
++ -
+
+Relacionais
+>, <, >=, <=, ==, ===, !
+
+Lógicos
+!
+&&
+!!
+
+Ternário
+?, :
+*/
+
+/* Operador ternário
+
+?
+:
+
+Exemplo
+idade = 10
+
+idade > 18 ? "Maior de idade" : "Menor de idade"
+*/
