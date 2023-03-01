e573241f519fb57f8cfabea6e9fba541a79d36c2
Guilherme Euclides do Amaral Silva <guilherme.silva@MacBook-Air-de-suporte1.local>
2023-03-01 12:28:13 -0300

    Add secao 8

---
+++ Curso javascript - udemy/8 - Manipulando a DOM/71 - Trabalhando com a DOM/script.js
@@ -0,0 +1,70 @@
+// Recuperar valor do componente com o id produto
+// let iTexto = document.getElementById('produto')
+// console.log(iTexto)
+
+
+
+// Recuperar valor dos componentes span
+// let spans = document.getElementsByTagName('span');
+// console.log(spans)
+
+
+
+// Recuperar valor dos componentes com a classe texto
+// let eles = document.getElementsByClassName('texto')
+// console.log(eles)
+
+
+
+// Ler valores
+// console.log(spans[0].textContent);
+// console.log(spans[1].innerHTML);
+
+
+
+// Alterar valores
+// spans[0].textContent = 'JavaScript';
+// spans[1].innerHTML = 'Geek University';
+
+
+
+// Ler e alterar
+// let span = document.getElementsByTagName('span') [0]
+// console.log(span.innerHTML)
+// span.style.textTransform = 'uppercase'
+
+
+// Ler valores com querySelector
+// let inp = document.querySelector('body div.container button') // Busca pela tag
+// console.log(inp.innerHTML)
+
+// let ele = document.querySelector('.texto') // Busca UM pela classe
+// console.log(ele)
+// let eles = document.querySelectorAll('.texto') // Busca TODOS pela classe
+// console.log(eles)
+
+// let div1 = document.querySelector('#div1') // Busca pelo ID
+// console.log(div1)
+
+// let inp2 = document.querySelector('input[name=produto]') // Busca pelo name
+// console.log(inp2)
+
+
+
+// Exemplo 1 - Evento no script
+// let btn = document.querySelector('button.btn');
+
+// btn.onclick = function (){
+//     alert('Botäo clicado...');
+// }
+
+
+
+// Exemplo 2 - Eventos no script
+// let btn = document.querySelector('button.btn');
+
+// let inp = document.querySelector('input[name=produto]');
+
+// btn.onclick = function(){
+//     alert(`Temos o texto:\n ${inp.value}`);
+// }
